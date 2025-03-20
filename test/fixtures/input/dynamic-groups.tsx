df679b780da4ec84f1ae3b585c0c1a9f6888d43b
asmirbe <asmir.belkic@gmail.com>
2025-03-20 02:11:02 +0100

    refactor(test): supprimer les fichiers de test obsolètes et nettoyer les imports

--- test/fixtures/input/dynamic-groups.tsx
+++
@@ -1,6 +0,0 @@
-// @app/dossier
-import AbsenceInitFormComponent  from '@app/dossier/components/absences/init/AbsenceInitFormComponent';
-// @app/client
-import useUtilisateurSearch      from '@app/client/providers/parametrage/utilisateurs/UtilisateurSearchProvider';
-// @app/notification
-import { useClientNotification } from '@app/notification/ClientNotificationProvider';
