aa9ef4bddfadfdfaf389cb82e0aaa79fa6e17774
asmirbe <asmir.belkic@gmail.com>
2025-03-20 02:11:02 +0100

    refactor(test): supprimer les fichiers de test obsolètes et nettoyer les imports

--- test/fixtures/expected/base.tsx
+++ test/fixtures/expected/base.tsx
@@ -1,9 +1,9 @@
 // Misc
-import React          from 'react';
-import { useState }   from 'react';
-import type Test      from 'react';
-import type { Test }  from 'react';
+import React         from 'react';
+import { useState }  from 'react';
+import type Test     from 'react';
+import type { Test } from 'react';
 
 // DS
-import { YpButton }  from 'ds';
+import { YpButton } from 'ds';
 
--- test/fixtures/expected/missing-comma.tsx
+++
@@ -1,8 +0,0 @@
-// @app/dossier
-import {
-    AbsenceInitFormComponent,
-    AbsencesFormComponent,
-    BritaniaFormComponent,
-    CaledoniaFormComponent,
-    DossierFormComponent
-}  from '@app/dossier/components/absences/init/AbsenceInitFormComponent';
