40d23e8f0b0ddf107dee4c475c63a4fe8d292d29
asmirbe <asmir.belkic@gmail.com>
2025-03-20 16:25:23 +0100

    refactor(formatteur): regrouper et trier les imports par type et source

--- test/fixtures/expected/base.tsx
+++ test/fixtures/expected/base.tsx
@@ -3,6 +3,8 @@ import React         from 'react';
 import { useState }  from 'react';
 import type Test     from 'react';
 import type { Test } from 'react';
+import { Button }    from 'antd';
+import 'style.css';
 
 // DS
 import { YpButton } from 'ds';
