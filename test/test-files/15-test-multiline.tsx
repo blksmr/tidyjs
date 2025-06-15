356e8a86bf16602eeeab3598a50c1519e806fdb2
asmirbe <asmir.belkic@gmail.com>
2025-06-15 21:25:37 +0200

    feat(extension): ajouter le pré-filtrage des imports manquants et inutilisés

---
+++ test/test-files/15-test-multiline.tsx
@@ -0,0 +1,21 @@
+// Misc
+import {
+    fetchData,
+    ApiResponse
+}                    from './api';
+import {
+    FC,
+    ReactNode
+}             from 'react';
+
+interface Props {
+  children: ReactNode;
+}
+
+export const TestMultiline: FC<Props> = ({ children }) => {
+  return (
+    <div>
+      {children}
+    </div>
+  );
+};
