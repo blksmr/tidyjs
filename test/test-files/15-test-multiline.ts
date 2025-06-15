67619e0e4ba1f1bfe1ce77d4f57df6f9f88b8125
asmirbe <asmir.belkic@gmail.com>
2025-06-15 18:18:36 +0200

    refactor(import-diagnostics): supprimer l'analyse des diagnostics d'importation feat(test): ajouter des fichiers de configuration pour TidyJS

---
+++ test/test-files/15-test-multiline.ts
@@ -0,0 +1,10 @@
+// Misc
+import {
+    fetchData,
+    ApiResponse
+}                    from './api';
+import { Component } from 'react';
+
+interface Props {
+  children: Component; // <- Type usage seulement
+}
